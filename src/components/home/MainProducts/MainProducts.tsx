6c78936786570a35f247855de1d49f9572ac38c7
Delta27222 <angelgabriel.hernandez@gmail.com>
2024-01-27 17:17:16 -0800

    Add MainProducts component

---
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -0,0 +1,8 @@
+export const MainProducts = () => {
+  return (
+    <section className="flex flex-col justify-center items-center p-24">
+      <h1 className="font-bold text-3xl">MainProducts</h1>
+    </section>
+  )
+}
+
