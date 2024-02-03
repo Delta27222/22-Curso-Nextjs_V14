1bd086413687ccd9792096afbc4ca867aa945beb
Delta27222 <angelgabriel.hernandez@gmail.com>
2024-02-03 15:54:39 -0800

    Update Hero component with new content

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,7 +1,8 @@
 export const Hero = () => {
   return (
-    <div className="flex flex-col justify-center items-center p-24">
-      <h1 className="font-bold text-3xl">Hero</h1>
-    </div>
+    <section className=''>
+      <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-pink-500 text-8xl my-12 mx-0 font-bold tracking-wider text-center">Future world</h1>
+      <h2 className="text-4xl mb-12 text-center text-secondary">Empowering Your Tomorrow, Today!</h2>
+    </section>
   )
 }
