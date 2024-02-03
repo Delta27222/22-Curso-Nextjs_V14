198f740d685c7dd315d55d37be8489d9807aa914
Delta27222 <angelgabriel.hernandez@gmail.com>
2024-02-03 15:54:43 -0800

    Refactor header navigation

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,16 +1,29 @@
 import Link from "next/link"
 
 export const Header = () => {
+  const links = [
+    {
+      name: "Home",
+      url: "/",
+    },
+    {
+      name: "Stock",
+      url: "/store",
+    },
+  ]
   return (
     <header>
       <nav>
-        <ul>
-        <Link href="/">
-            <li>Home</li>
-          </Link>
-          <Link href="/store">
-            <li>Stock</li>
-          </Link>
+        <ul className="flex flex-row flex-nowrap list-none gap-10 my-0 mx-auto p-8 justify-center">
+          {links.map((link) => {
+            return (
+              <li key={link.url}>
+                <Link className="list-none text-xl font-normal bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-purple-500" href={link.url}>
+                  {link.name}
+                </Link>
+              </li>
+            )
+          })}
         </ul>
       </nav>
     </header>
