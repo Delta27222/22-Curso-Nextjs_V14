15af272a4b9117199415aab17be7e008fc61b9a5
Delta27222 <angelgabriel.hernandez@gmail.com>
2024-02-06 07:14:25 -0800

    Refactor Header component to improve link styling

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -17,8 +17,8 @@ export const Header = () => {
         <ul className="flex flex-row flex-nowrap list-none gap-10 my-0 mx-auto p-8 justify-center">
           {links.map((link) => {
             return (
-              <li key={link.url}>
-                <Link className="list-none text-xl font-normal bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-purple-500" href={link.url}>
+              <li key={link.url} className="list-none text-xl font-normal bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-purple-500">
+                <Link href={link.url}  scroll={false}>
                   {link.name}
                 </Link>
               </li>
