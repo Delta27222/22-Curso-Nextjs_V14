02fb45427eaad71a22d6b3057972f203de0d7990
Delta27222 <angelgabriel.hernandez@gmail.com>
2024-02-06 08:31:26 -0800

    Add home page component

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,10 @@
+import { MainProducts } from "app/components/home/MainProducts/MainProducts";
+
+function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center">
+      <MainProducts />
+    </main>
+  );
+}
+export default Home;
