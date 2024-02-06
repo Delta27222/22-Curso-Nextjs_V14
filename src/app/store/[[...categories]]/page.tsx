3a5c7a29827fb1284c49f0b5746825ef0af7520b
Delta27222 <angelgabriel.hernandez@gmail.com>
2024-02-06 11:16:09 -0800

    Remove unnecessary error throwing statement in Category component

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -9,7 +9,6 @@ interface CategoriesProps {
 const Category = (props: CategoriesProps) => {
   const { categories } = props.params;
   const params = props.searchParams;
-  throw new Error('Error: ' + JSON.stringify(params));     //TODO -> Aca esta el error  mi pana
   return (
     <div className="flex flex-col justify-center items-center p-24">
       <div className="font-bold text-3xl">Dinamic Category: {categories}</div>
