27fddd3c87458694e4b1a38b42a39e621ddfc318
Delta27222 <angelgabriel.hernandez@gmail.com>
2024-02-07 14:29:07 -0800

    Add ProductType interface to types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -2,3 +2,16 @@ export interface ErrorPageProps {
   error: Error,
   reset: () => void;
 }
+
+type ProductType = {
+  id: string;
+  title: string;
+  description: string;
+  variants: {
+    price: number;
+  }[];
+  image: string;
+  quantity: number;
+  handle: string;
+  tags: string;
+};
