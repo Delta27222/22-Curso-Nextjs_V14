d272e5a550aa2ba9b5c0cbf24b23544aeb3bb5d0
Delta27222 <angelgabriel.hernandez@gmail.com>
2024-02-07 06:59:29 -0800

    Add getProducts function to fetch products from Shopify API

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,19 +1,5 @@
+import { getProducts } from "app/services/shopify";
 import Image from "next/image";
-
-const getProducts = async () => {
-  try {
-    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}admin/api/2023-10/products.json`, {
-      headers: new Headers( {
-        "X-Shopify-Access-Token": process.env.SHOPIFY_API_KEY || ''
-      })
-    });
-    const { products } = await response.json();
-    return products;
-  }
-  catch (error) {
-    console.log(error);
-  }
-}
 
 interface Product {
   id: string;
